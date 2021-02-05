ba9e3575f28c6231806252239b3ba615275d9ba3
andres-briones <Nona9614@hotmail.com>
2021-02-04 19:31:11 -0600

    Creating support files to easy access to constant paths and files in the project

---
+++ VBA CLI/Files.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VBA.Project
+{
+    public static class Files
+    {
+        public static string CustomUI{ get { return $@"{Paths.Resources}/customUI.xml"; } }
+    }
+}
+namespace VBA.Executable
+{
+    public static class Files
+    {
+        public static string CustomUI { get { return $@"{Paths.Resources}/customUI.xml"; } }
+        public static string CallbacksModule { get { return $@"{Paths.Resources}/Callbacks.bas"; } }
+        public static string Rels { get { return $@"{Paths.Resources}/.rels"; } }
+        public static string HelpContent { get { return $@"{Paths.Resources}/help.json"; } }
+    }
+}
---
+++ VBA CLI/Paths.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using System.IO;
+
+namespace VBA.Project
+{
+    public static class Paths
+    {
+#if (DEBUG)
+        public static string Base { get { return @"D:/Documents/Personal/repos/apps/vba-cli/VBA CLI"; } }
+#else
+        public static string Base { get { return Directory.GetCurrentDirectory(); } }
+#endif
+        public static string Resources { get { return $@"{Base}/Resources"; } }
+    }
+}
+namespace VBA.Executable
+{
+    public static class Paths
+    {
+        public static string Base { get { return Assembly.GetExecutingAssembly().CodeBase.Replace($"/{Assembly.GetExecutingAssembly().GetName().Name}.dll", "").Replace("file:///", "").Replace(@"/bin/Release/netcoreapp3.1", ""); } }
+        public static string Resources { get { return $@"{Base}/Resources"; } }
+    }
+}
