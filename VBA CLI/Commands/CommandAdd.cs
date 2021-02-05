68c10f3ff9b54ec525eac03d02ee4bfc95a8dcdb
andres-briones <Nona9614@hotmail.com>
2021-02-04 19:45:41 -0600

    Adding handler of xml file for the project

--- VBA CLI/Commands/CommandAdd.cs
+++ VBA CLI/Commands/CommandAdd.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 using Excel = VBA.ExcelHandler;
 
 namespace VBA.Switches
@@ -20,33 +22,60 @@ namespace VBA.Switches
 
         public bool Call(List<string> parameters)
         {
-            bool result;
+            if (parameters == null)
+            {
+                Console.WriteLine("This command needs parameters");
+                return false;
+            }
+            bool result = false;
             switch (parameters[0])
             {
                 case "customUI":
-                    // Checks if a directory value was set
-                    if (parameters.Count == 3)
+                    string customUI = null;
+                    string excel = null;
+                    bool fileNotFound = false;
+                    switch (parameters.Count)
                     {
-                        result = Excel.AddCustomUI(parameters[1], parameters[2], true);
+                        case 2:
+                            // CustomUI file name was not set, default will be used
+                            customUI = @$"{Project.Files.CustomUI}";
+                            excel = SetDefaultPath(parameters[1], $"{Directory.GetCurrentDirectory()}");
+                            break;
+                        case 3:
+                            customUI = SetDefaultPath(parameters[2], $@"{Directory.GetCurrentDirectory()}\resources");
+                            excel = SetDefaultPath(parameters[1], Directory.GetCurrentDirectory());
+                            break;
+                        default:
+                            Console.WriteLine("Not recognized parameters");
+                            break;
                     }
-                    else
+                    if (!fileNotFound)
                     {
-                        Console.WriteLine("Not enough parameters");
-                        result = false;
+                        result = Excel.AddCustomUI(excel, customUI);
                     }
                     break;
                 default:
                     Console.WriteLine($"Option '{parameters[0]}' is not valid");
-                    result = false;
                     break;
             }
             return result;
         }
-
+        // If it is a valid file name with no path, a default will be used
+        private static string SetDefaultPath(string name, string path)
+        {
+            // Checks if the file name comes alone or with a path
+            if (File.Exists(name))
+            {
+                return name;
+            }
+            else
+            {
+                return name.IndexOfAny(Path.GetInvalidFileNameChars()) < 0 ? $@"{path}\{name}" : null;
+            }
+        }
         public void Dispose()
         {
             instance.Dispose();
         }
-
     }
 }
---
+++ VBA CLI/Handlers/XMLHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace VBA.Handlers
+{
+    public static class XMLHandler
+    {
+        public static bool GenerateDefaultCustomUI()
+        {
+            // Validate overriding
+            if (File.Exists(Project.Files.CustomUI))
+            {
+                Console.Write("There is a customUI file already created. \nWould you like to override it? (y/n) --> ");
+                if (!(Regex.Match(Console.ReadLine().Trim(), "^y*").Length > 0))
+                {
+                    Console.WriteLine("Proccess Canceled");
+                    return false;
+                }
+                else
+                {
+                    File.Delete(Project.Files.CustomUI);
+                    File.Copy(Executable.Files.CustomUI, Project.Files.CustomUI);
+                    Console.WriteLine("CustomUI file created properly");
+                }
+            }
+            else
+            {
+                Directory.CreateDirectory(Project.Paths.Resources);
+                File.Copy(Executable.Files.CustomUI, Project.Files.CustomUI);
+                Console.WriteLine("CustomUI file created successfully");
+            }
+            return true;
+        }
+
+    }
+}
