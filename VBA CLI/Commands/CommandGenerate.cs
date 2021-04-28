41088856d7b54143fc49fdeee328d8b720dff5a2
andres-briones <carlos.briones@bcstechnology.com.au>
2021-04-27 23:03:49 -0500

    Improving command generate

--- VBA CLI/Commands/CommandGenerate.cs
+++ VBA CLI/Commands/CommandGenerate.cs
@@ -1,8 +1,7 @@
 ﻿using System.Collections.Generic;
-using Excel = VBA.ExcelHandler;
 using System;
-using System.Text.RegularExpressions;
 using VBA.Handlers;
+using VBA.Project;
 
 namespace VBA.Switches
 {
@@ -26,28 +25,67 @@ namespace VBA.Switches
                 return false;
             }
             bool result = false;
+            string name = null;
+            string _base = null;
+            string customUI = null;
             switch (parameters[0])
             {
                 case "customUI":
                     XMLHandler.GenerateDefaultCustomUI();
                     break;
                 case "excel-file":
-                    string name = null;
                     switch (parameters.Count)
                     {
+                        // If not name is asigned, excel will try to create 'project.xlsm' file
+                        case 1:
+                            name = "project";
+                            _base = Paths.Base;
+                            break;
                         case 2:
                             name = parameters[1];
-                            name = Regex.Match(name, @"\w\.*").Length < 0 ? $"{name}.xlsm" : name;
-                            name = Project.Paths.CheckForDefaultPath(name);
+                            _base = Paths.Base;
+                            break;
+                        case 3:
+                            name = parameters[1];
+                            _base = parameters[2];
+                            break;
+                        case 4:
+                            name = parameters[1];
+                            _base = parameters[2];
+                            customUI = parameters[3];
+                            break;
+                        default:
+                            Console.WriteLine("Not recognized parameters");
+                            break;
+                    }
+                    result = GeneratorHandler.CreateExcelFile(name, _base, customUI);
+                    break;
+                case "project":
+                    switch (parameters.Count)
+                    {
+                        // If not name is asigned, excel will try to create 'project.xlsm' file
+                        case 1:
+                            name = "project";
+                            _base = Paths.Base;
+                            break;
+                        case 2:
+                            name = parameters[1];
+                            _base = Paths.Base;
+                            break;
+                        case 3:
+                            name = parameters[1];
+                            _base = parameters[2];
+                            break;
+                        case 4:
+                            name = parameters[1];
+                            _base = parameters[2];
+                            customUI = parameters[3];
                             break;
                         default:
                             Console.WriteLine("Not recognized parameters"); 
                             break;
                     }
-                    if (name != null) 
-                    {
-                        result = Excel.CreateExcelFile(name); 
-                    }
+                    result = GeneratorHandler.CreateProject(name, _base, customUI);
                     break;
                 default:
                     Console.WriteLine($"Option '{parameters[0]}' is not valid");
