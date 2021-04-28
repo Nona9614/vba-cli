879bcf59af724a40941df0938c94fc2e62cf89ca
andres-briones <carlos.briones@bcstechnology.com.au>
2021-04-28 00:10:48 -0500

    Changing all forward slashes for backward slashes

--- VBA CLI/Files.cs
+++ VBA CLI/Files.cs
@@ -10,18 +10,18 @@ namespace VBA.Project
         {
             public static class Classes
             {
-                public static string Default { get { return $@"{Paths.VBE.Classes}/Class.cls"; } }
-                public static string StaticClass { get { return $@"{Paths.VBE.Classes}/StaticClass.cls"; } }
-                public static string Interface { get { return $@"{Paths.VBE.Classes}/Interface.cls"; } }
+                public static string Default { get { return $@"{Paths.VBE.Classes}\Class.cls"; } }
+                public static string StaticClass { get { return $@"{Paths.VBE.Classes}\StaticClass.cls"; } }
+                public static string Interface { get { return $@"{Paths.VBE.Classes}\Interface.cls"; } }
             }
             public static class CustomUI
             {
-                public static string Default { get { return $@"{Paths.VBE.CustomUI}/customUI.xml"; } }
+                public static string Default { get { return $@"{Paths.VBE.CustomUI}\customUI.xml"; } }
             }
             public static class Modules
             {
-                public static string Default { get { return $@"{Paths.VBE.Modules}/Module.bas"; } }
-                public static string Callbacks { get { return $@"{Paths.VBE.Modules}/Callbacks.bas"; } }
+                public static string Default { get { return $@"{Paths.VBE.Modules}\Module.bas"; } }
+                public static string Callbacks { get { return $@"{Paths.VBE.Modules}\Callbacks.bas"; } }
             }
         }
     }
@@ -34,24 +34,24 @@ namespace VBA.Executable
         {
             public static class Classes
             {
-                public static string Default { get { return $@"{Paths.Resources}/VBE/Classes/Class.cls"; } }
-                public static string StaticClass { get { return $@"{Paths.Resources}/VBE/Classes/StaticClass.cls"; } }
-                public static string Interface { get { return $@"{Paths.Resources}/VBE/Classes/Interface.cls"; } }
+                public static string Default { get { return $@"{Paths.Resources}\VBE\Classes\Class.cls"; } }
+                public static string StaticClass { get { return $@"{Paths.Resources}\VBE\Classes\StaticClass.cls"; } }
+                public static string Interface { get { return $@"{Paths.Resources}\VBE\Classes\Interface.cls"; } }
             }
             public static class CustomUI
             {
-                public static string Rels { get { return $@"{Paths.Resources}/VBE/CustomUI/.rels"; } }
-                public static string Default { get { return $@"{Paths.Resources}/VBE/CustomUI/customUI.xml"; } }
+                public static string Rels { get { return $@"{Paths.Resources}\VBE\CustomUI\.rels"; } }
+                public static string Default { get { return $@"{Paths.Resources}\VBE\CustomUI\customUI.xml"; } }
             }
             public static class Modules
             {
-                public static string Default { get { return $@"{Paths.Resources}/VBE/Modules/Module.bas"; } }
-                public static string Callbacks { get { return $@"{Paths.Resources}/VBE/Modules/Callbacks.bas"; } }
+                public static string Default { get { return $@"{Paths.Resources}\VBE\Modules\Module.bas"; } }
+                public static string Callbacks { get { return $@"{Paths.Resources}\VBE\Modules\Callbacks.bas"; } }
             }
         }
         public static class CLI
         {
-            public static string HelpContent { get { return $@"{Paths.Resources}/help.json"; } }
+            public static string HelpContent { get { return $@"{Paths.Resources}\help.json"; } }
         }
     }
 }
--- VBA CLI/Paths.cs
+++ VBA CLI/Paths.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Reflection;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace VBA.Project
 {
@@ -52,33 +53,28 @@ namespace VBA.Project
             }
             else
             {
-                return name.IndexOfAny(Path.GetInvalidFileNameChars()) < 0 ? $@"{Base}/{name}" : null;
+                return name.IndexOfAny(Path.GetInvalidFileNameChars()) < 0 ? $@"{Base}\{name}" : null;
             }
         }
 
 #if (DEBUG)
-        private static string _base = @"D:/Documents/Personal/repos/apps/vba-cli/VBA CLI";
+        private static string _base = @"D:\Documents\Personal\repos\apps\vba-cli\VBA CLI";
+#else
+        private static string _base = Directory.GetCurrentDirectory();
+#endif
         public static string Base
-        { 
+        {
             get { return _base; }
             set { _base = value; }
         }
-#else
-        private static string _base = Directory.GetCurrentDirectory();
-        public static string Base 
-        { 
-            get { return Directory.GetCurrentDirectory(); } 
-            set { _base = value; }
-        }
-#endif
-        public static string Resources { get { return $@"{Base}/resources"; } }
+        public static string Resources { get { return $@"{Base}\resources"; } }
 
         public static class VBE
         {
-            public static string Forms { get { return $@"{Resources}/VBE/forms"; } }
-            public static string Modules { get { return $@"{Resources}/VBE/modules"; } }
-            public static string Classes { get { return $@"{Resources}/VBE/classes"; } }
-            public static string CustomUI { get { return $@"{Resources}/VBE/customUI"; } }
+            public static string Forms { get { return $@"{Resources}\forms"; } }
+            public static string Modules { get { return $@"{Resources}\modules"; } }
+            public static string Classes { get { return $@"{Resources}\classes"; } }
+            public static string CustomUI { get { return $@"{Resources}\customUI"; } }
         }
     }
 }
@@ -86,12 +82,19 @@ namespace VBA.Executable
 {
     public static class Paths
     {
+        public static string Base { get {
+                string _name = Assembly.GetExecutingAssembly().GetName().Name;
+                string _codebase = Regex.Replace(Assembly.GetExecutingAssembly().CodeBase, "[/]", "\\");
 #if DEBUG
-        public static string Base { get { return Assembly.GetExecutingAssembly().CodeBase.Replace($"/{Assembly.GetExecutingAssembly().GetName().Name}.dll", "").Replace("file:///", "").Replace(@"/bin/Debug/netcoreapp3.1", ""); } }
+                string _bin = @"\bin\Release\netcoreapp3.1\";
 #else
-        public static string Base { get { return Assembly.GetExecutingAssembly().CodeBase.Replace($"/{Assembly.GetExecutingAssembly().GetName().Name}.dll", "").Replace("file:///", "").Replace(@"/bin/Release/netcoreapp3.1", ""); } }
+                // If the program is installed uses the exe path as base
+                string _bin = @"\bin\Release\netcoreapp3.1\";
 #endif
-        public static string Resources { get { return $@"{Base}/Resources"; } }
+                string _uri = @"file:\\\";
+                return _codebase.Replace(_uri, "").Replace(@$"{_bin}{_name}.dll", ""); } 
+        }
+        public static string Resources { get { return $@"{Base}\Resources"; } }
     }
 
 }
