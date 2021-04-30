9c914b669fe09c6a70ac2ae7597925b46bd67d16
andres-briones <carlos.briones@bcstechnology.com.au>
2021-04-30 15:59:56 -0500

    Adding handler for cases where parameters comes with switches

---
+++ VBA CLI/Handlers/SwitchesHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VBA.Handlers
+{
+    public static class SwitchesHandler
+    {
+        private const string ONLY_EXECUTABLE_SWITCH = "/e";
+        public static sbyte UsesExecutablePaths(ref List<string> parameters)
+        {
+            if (parameters == null) return -1;
+            if (parameters.Contains(ONLY_EXECUTABLE_SWITCH))
+            {
+                Console.WriteLine("Using default files");
+                parameters.Remove(ONLY_EXECUTABLE_SWITCH);
+                return 1;
+            }
+            else return 0;
+        }
+    }
+}
